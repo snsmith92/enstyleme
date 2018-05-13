b3348247785c07b8e15962e0e0052ac3af5be006
snsmith92 <sherzelsmith@gmail.com>
2018-05-13 21:36:47 +0200

    Added admin log in via devise and associated views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   devise_for :users
   root "static_pages#index"
   get 'start', to: 'static_pages#start'
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
+  factory :admin do
+    
+  end
   factory :tagging do
     tag nil
     service nil
