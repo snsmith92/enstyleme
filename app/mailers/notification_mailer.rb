ebd2a6c79b44bb737b08f394ac317c3421f8b636
snsmith92 <sherzelsmith@gmail.com>
2018-05-15 15:54:33 +0200

    Added mailers to vendor create action

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,10 @@
+class NotificationMailer < ApplicationMailer
+  default from: "notifications@enstyle.me"
+
+  def vendor_created(vendor)
+    @user = vendor.user
+    mail(to: @user.email,
+      subject: "Welcome to Enstyle.me #{vendor.name}!")
+  end 
+
+end
