21fcfbcffa456661a2107b76ffd851bfefc2d522
snsmith92 <sherzelsmith@gmail.com>
2018-05-31 22:34:56 +0200

    Fixed rspec tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,4 @@
 FactoryBot.define do
-  factory :category do
-    
-  end
   factory :admin do
     
   end
@@ -24,8 +21,15 @@ FactoryBot.define do
     association :vendor
     association :user
   end
+
+  factory :category do 
+    
+  end 
   
   factory :vendor do
+    association :category, factory: :category
+    first_name "Zel"
+    last_name "Williams"
     name "Sherzel's Nail Spa"
     country "BS"
     city "Nassau"
