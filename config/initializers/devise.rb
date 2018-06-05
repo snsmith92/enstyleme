49ff7fd4b7fad4825a17882b1d21051f350f350f
snsmith92 <sherzelsmith@gmail.com>
2018-06-05 14:25:16 +0200

    Assets precompile

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -259,6 +259,8 @@ Devise.setup do |config|
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], scope: 'email', info_fields: 'email'
 
+  config.secret_key = ENV["DEVISE_SECRET_KEY"]
+
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
