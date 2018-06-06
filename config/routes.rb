e49995e6abc687fcf68e882427f622fd678d5332
snsmith92 <sherzelsmith@gmail.com>
2018-06-06 11:26:14 +0200

    Adjusted devise password and confirmation views, and redirect on sign up to start_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admins
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations'}
   root "static_pages#index"
   get 'start', to: 'static_pages#start'
   get 'business', to: 'static_pages#business'
