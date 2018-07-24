13bd198043289479988f48ceb56f6a79a5ad96fc
snsmith92 <sherzelsmith@gmail.com>
2018-07-24 15:08:29 +0200

    Addressed database issues

--- db/migrate/20180609080328_change_day_to_integer_availabilities.rb
+++ db/migrate/20180609080328_change_day_to_integer_availabilities.rb
@@ -1,7 +1,5 @@
 class ChangeDayToIntegerAvailabilities < ActiveRecord::Migration[5.0]
   def change
-    remove_column :availabilities, :available
-    remove_column :availabilities, :day
     add_column :availabilities, :day, :integer
   end
 end
